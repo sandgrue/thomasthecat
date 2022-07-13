97884ecde02298eb7e684763a9dda448cacc9567
mayank8120 <mayanklaxkar11@gmail.com>
2022-07-13 15:33:29 +0530

    EZOIC ads component

--- src/containers/GoogleADS.js
+++ src/containers/GoogleADS.js
@@ -12,20 +12,20 @@ const GoogleADS = ({ placeholderId, width, height, slotno }) => {
     return (
         <>
             <div id={`ezoic-pub-ad-placeholder-${placeholderId}`}>
-                <ins class="adsbygoogle"
+                {/* <ins class="adsbygoogle"
                     style={{
                         display: "inline-block", width: `${width}`, height: `${height}`
                     }}
                     data-ad-client={GOOGLEADSCLIENTID}
-                    data-ad-slot={slotno}></ins>
+                    data-ad-slot={slotno}></ins> */}
 
 
                 <h1>hiiii</h1>
 
-                {/* <ins class="adsbygoogle"
+                <ins class="adsbygoogle"
                     style={{ display: "inline-block", width: "728px", height: "90px" }}
                     data-ad-client="ca-pub-2836217214879103"
-                    data-ad-slot="6662484805"></ins> */}
+                    data-ad-slot="6662484805"></ins>
             </div>
         </>
     )
